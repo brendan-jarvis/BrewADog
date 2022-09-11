d47ecaa4f9c0e26c6ab992669830450496db9c4f
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-09-11 15:54:35 +1200

    Add conversion functions to Utils.js

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 import ErrorMessage from './ErrorMessage'
 import Form from './Form'
+import {
+  calculateMCU,
+  calculateSRM,
+  convertEBCToSRM,
+  convertSRMToEBC,
+  convertLitresToGallons,
+  convertKilogramsToPounds,
+} from './Utils'
 
 function App() {
+  // Tests
+  console.log('Expect a beer with SRM 10 to have EBC of 20')
+  console.log('Converting SRM 10 to EBC, result: ', convertSRMToEBC(10))
+  console.log('Converting EBC 20 to SRM, result: ', convertEBCToSRM(20))
+  console.log(
+    'Converting 20 litres to gallons, result: ',
+    convertLitresToGallons(20)
+  )
+  console.log(
+    'Converting 5 kg to pounds, result: ',
+    convertKilogramsToPounds(5)
+  )
+
   return (
     <div>
       <ErrorMessage />
---
+++ client/components/Utils.js
@@ -0,0 +1,32 @@
+const kgToPounds = 2.20462 // 1 KG = 2.20462 lbs
+
+const litresToGallons = 0.264172 // 1 L = 0.264172 gallons
+
+export const convertKilogramsToPounds = (grainWeight) =>
+  grainWeight * kgToPounds
+export const convertLitresToGallons = (volume) => volume * litresToGallons
+
+export function calculateMCU(grainWeight, grainColour, volume) {
+  // MCU = (Weight of grain in lbs) * (Colour of grain in degrees Lovibond) / (volume in gallons)
+  return (
+    (convertKilogramsToPounds(grainWeight) * grainColour) /
+    convertLitresToGallons(volume)
+  )
+}
+
+export function calculateSRM(MCU) {
+  // For multiple grain additions, calculate MCU for each addition and add them together
+  // SRM colour = 1.4922 * (MCU * 0.6859)
+  // Morey Equation - an excellent estimate of beer colour through the range of 1-50 SRM
+  return 1.4922 * (MCU * 0.6859)
+}
+
+export function convertSRMToEBC(SRM) {
+  // EBC = SRM * 1.97
+  return SRM * 1.97
+}
+
+export function convertEBCToSRM(EBC) {
+  // SRM = EBC * 0.508
+  return EBC * 0.508
+}
